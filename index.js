fc3f0ec93f219c8da23c02497aec6dcd8a17f225
Bret Little <bret.little@gmail.com>
2014-07-25 17:47:36 -0600

    Update api

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ var app = express();
 var temples = require('./temples');
 
 app.get('/temples', function(req, res){
-  res.send(JSON.stringify(temples));
+  res.send(JSON.stringify(
+        {
+          "current_page": 1,
+          "total_pages": 284,
+          "total_items": 11352,
+          "photos": temples
+        }
+    ));
 });
 
 var server = app.listen(8080, function() {
