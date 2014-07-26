a536048a0a9b2349aeb42528df554f021072b070
Bret Little <bret.little@gmail.com>
2014-07-26 17:02:37 -0600

    Add logging

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 var express = require('express');
+var winston = require('winston');
+var dateFormat = require('dateformat');
+
 var app = express();
+var now = new Date();
+
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: dateFormat(now, "ddmmyyyy") + '.log' })
+    ]
+});
 
 var temples = require('./temples');
 
 app.get('/temples', function(req, res){
-  res.send(JSON.stringify(
-        {
-          "current_page": 1,
-          "total_pages": 284,
-          "total_items": 11352,
-          "photos": temples
-        }
-    ));
+    logger.info('GET /temples');
+
+    res.send(JSON.stringify(
+    {
+      "current_page": 1,
+      "total_pages": 1,
+      "total_items": temples.length,
+      "photos": temples
+    }
+  ));
 });
 
 var server = app.listen(8080, function() {
-    console.log('Listening on port %d', server.address().port);
+    console.info('Listening on port %d', server.address().port);
 });
